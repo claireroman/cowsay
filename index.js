ed6b842189d2bc32eac8a6ad906412445ff109ef
Claire Roman <claire.roman.08@orange.fr>
2022-01-19 15:55:24 +0100

    correction de l'erreur dans le texte : manque name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const person = require('./information');
 const cowsay = require('cowsay');
 
 console.log(cowsay.say({
-    text : `Hi, my is ${person.name} and my campus is based in ${person.campus}.`,
+    text : `Hi, my name is ${person.name} and my campus is based in ${person.campus}.`,
     e : 'oO',
     T : 'U'
 }));
